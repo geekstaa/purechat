bb0f21117d0916b5b16a651fc80fb6e7603ac2a9
geekstaa <33970101+geekstaa@users.noreply.github.com>
2017-11-25 01:25:26 +0000

    Update projectchat.js

--- projectchat.js
+++ projectchat.js
@@ -1,4 +1,3 @@
-<html>
-<head>
-    <script src="https://raw.github.com/geekstaa/purechat/master/projectchat/AdvancedPopup/jquery.min.js"></script>
-</head>
+<script>
+  $.getScript("https://raw.github.com/geekstaa/purechat/master/chat/script.js");
+</script>
